ecf4faad30b09fe7e1ed95c9d66258eab6139206
Maxime Moreillon <m.moreillon@gmail.com>
2022-05-12 16:23:14 +0900

    routers

--- routers/directors.py
+++ routers/directors.py
@@ -4,32 +4,36 @@ from typing import List
 
 from ..main import get_db
 from ..controllers.director import create_director, get_director, get_directors 
+from ..controllers.movie import create_movie 
+
 from ..schemas.director import Director, DirectorCreate
 from ..schemas.movie import Movie, MovieCreate
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/directors",
+)
 
 
 
-@router.post("/directors/", response_model=Director)
+@router.post("/", response_model=Director)
 def create_user(director: DirectorCreate, db: Session = Depends(get_db)):
     return create_director(db=db, director=director)
 
 
-@router.get("/directors/", response_model=List[Director])
+@router.get("/", response_model=List[Director])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     directors = get_directors(db, skip=skip, limit=limit)
     return directors
 
 
-@router.get("/directors/{director_id}", response_model=Director)
+@router.get("/{director_id}", response_model=Director)
 def read_user(id: int, db: Session = Depends(get_db)):
-    db_user = get_director(db, id=id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+    director = get_director(db, id=id)
+    if director is None:
+        raise HTTPException(status_code=404, detail="Director not found")
+    return director
 
 
-@router.post("/directors/{director_id}/movies/", response_model=Movie)
-def create_item_for_user( user_id: int, item: MovieCreate, db: Session = Depends(get_db) ):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
+@router.post("/{director_id}/movies/", response_model=Movie)
+def create_item_for_user( director_id: int, movie: MovieCreate, db: Session = Depends(get_db) ):
+    return create_movie(db=db, movie=movie, director_id=director_id)
---
+++ routers/movies.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter,Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from ..main import get_db
+from ..controllers.movie import get_movie, get_movies
+
+from ..schemas.movie import Movie, MovieCreate
+
+router = APIRouter(
+    prefix="/movies",
+)
+
+
+@router.get("/", response_model=list[Movie])
+def read_movies(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    movies = get_movies(db, skip=skip, limit=limit)
+    return movies
+
+@router.get("/{movie_id}", response_model=Movie)
+def read_movies(id: int, db: Session = Depends(get_db)):
+    movie = get_movie(db, id=id)
+    return items
